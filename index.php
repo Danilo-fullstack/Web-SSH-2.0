6e18e9799aeada86c687d2eedb19d8092aebe8e5
danilo <danilosantosdeoliveia40@gmail.com>
2020-03-17 21:18:29 -0300

    update-home

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web SSH</title>
+    <script src="https://kit.fontawesome.com/7d2e722761.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/feather-icons"></script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="_css/reset.css">
+    <link rel="stylesheet" href="_css/style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <h1 class="logo">
+          <a href="index.php">Web <span>SSH</span></a>
+        </h1>
+        <ul>
+          <li>
+            <a href="index.php" class="i">
+                <i class="fas fa-home"></i>Início
+            </a>
+          </li>
+          <li>
+            <a href="tutoriais.php" class="i">
+                <i class="far fa-play-circle"></i>Tutoriais
+            </a>
+          </li>
+          <li>
+            <a href="contato.php" class="i">
+                <i class="far fa-address-book"></i>Contato
+            </a>
+          </li>
+          <li>
+            <a href="login.php" class="i">
+                <i class="fas fa-sign-in-alt"></i>Login
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="box-welcome">
+        <h3>Seja bem vindo a Web SSH</h3>
+        <div class="i comprar-agora">
+          <a href="#">
+            <i class="fas fa-shopping-cart"></i>
+            Comprar Agora
+          </a>
+        </div>
+      </div>
+    </header>
+    <section>
+
+    </section>
+  </body>
+</html>
