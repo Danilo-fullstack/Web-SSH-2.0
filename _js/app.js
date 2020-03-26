938f64e7cdac0312b89fa26e57fb8b9b63461679
danilo <danilosantosdeoliveia40@gmail.com>
2020-03-26 00:47:49 -0300

    update-menu-mobile

--- _js/app.js
+++ _js/app.js
@@ -1,3 +1,39 @@
+// Verificar se o checkbox está checado e deixa a logo e o burger position: absolute
+
+var check = document.getElementById("btn-menu")
+var logo = document.querySelector(".logo")
+var burger = document.querySelector(".burger")
+
+check.addEventListener('click',function() {
+  if (check.checked == true){
+    logo.style.position = "fixed"
+    burger.style.position = "fixed"
+    check.style.position = "fixed"
+  } else {
+    logo.style.position = "absolute"
+    burger.style.position = "absolute"
+    check.style.position = "absolute"
+  }
+})
+
+//-----------------------------------------------------------------//
+
+
+
+
+
+
+
+/*
+  if (check.checked == true){ 
+    window.alert('CHECOU, DEU CERTO!!!');
+  }
+*/
+
+//var botao = document.querySelector('.btn-menu')
+//window.alert(botao);
+
+/*
 var menu = document.querySelector('.side-bar-mobile')
 var burger = document.querySelector('.burger')
 
@@ -8,6 +44,7 @@ if
 burger.addEventListener('click',function() {
     menu.classList.remove('menu-ativo')
 })
+*/
 
 
 
